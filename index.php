a2f61b66cb62ee6ed9675cf1633e673d3e7e44b3
RaghuGamesHD <52646455+RaghuGamesHD@users.noreply.github.com>
2019-07-08 15:09:28 +0800

    Created favicon
    Created a simple favicon for the site

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html>
+<head>
+<title> Hotel ABC: Home </title>
+<link rel = "stylesheet" type = "text/css" href = "assets/css/ui.calendars.picker.css">
+<link rel="shortcut icon" href="assets/images/icons/favicon.ico" type="image/x-icon">
+</head>
 <body>
 
 <?php

39fc372ece5d3ef9a6b2f7242c7e0d6237bbf5e2
TapanThakar14 <TapanThakar14@gmail.com>
2019-08-11 18:03:30 +0800

    Location change
    Changed the contact page map location from new york to singapore

--- front/contact.php
+++ front/contact.php
@@ -40,7 +40,7 @@
 <!--map start here-->
 <div class="map">
   <div class="container">
-	 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387144.007583421!2d-73.97800349999999!3d40.7056308!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY%2C+USA!5e0!3m2!1sen!2sin!4v1415253431785"  frameborder="0" style="border:0"> </iframe>
+	 <iframe src="https://www.google.com/maps/place/Singapore/@1.3139961,103.7041631,11z/data=!3m1!4b1!4m5!3m4!1s0x31da11238a8b9375:0x887869cf52abf5c4!8m2!3d1.352083!4d103.819836"  frameborder="0" style="border:0"> </iframe>
  </div>
 </div>
 <!--map end here-->

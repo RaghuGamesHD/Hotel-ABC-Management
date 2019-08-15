54c5f2750e44602301fb4b55bf2e37ec85109240
RaghuGamesHD <52646455+RaghuGamesHD@users.noreply.github.com>
2019-08-15 23:14:32 +0800

    abcms logo, modified back index footer

--- back/dist/index.php
+++ back/dist/index.php
@@ -111,7 +111,7 @@
                             </div>
                         </div>
                     </div>
-                    More Templates <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">模板之家</a> - Collect from <a href="http://www.cssmoban.com/" title="网页模板" target="_blank">网页模板</a>
+                    Copyright © 2019 Hotel ABC All rights reserved.
                 </div>
             </div>
         </div>
